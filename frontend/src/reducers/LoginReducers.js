ee1907a85de2379e887cdf4e0f009a4f64d9930f
amanc1248 <amanc1248@gmail.com>
2022-07-19 16:37:31 +0545

    Add: Login reducers

---
+++ frontend/src/reducers/LoginReducers.js
@@ -0,0 +1,19 @@
+import {
+  USER__LOGIN,
+  USER__LOGIN__FAILURE,
+  USER__LOGIN__SUCCESS,
+} from "../constants/loginconstants";
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER__LOGIN:
+      return { loading: true };
+    case USER__LOGIN__SUCCESS:
+      return { loading: false, userData: action.payload };
+    case USER__LOGIN__FAILURE:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
